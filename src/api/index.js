aec8889ac6c1395c262e8cb0b19a924ab85bdbf6
Bakar002 <abubakaramjad78600@gmail.com>
2025-01-30 17:33:53 +0500

    addinf

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+
+  baseURL: "https://projectbackend-production-2aa3.up.railway.app/api/",
+
 });
 //admin
 export const insertFlight = (payload) =>
